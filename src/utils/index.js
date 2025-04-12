198b1d5a1db16778d89be602dba3d3a2be886309
haider7b <alasdyhydr413@gmail.com>
2025-04-12 16:12:31 +0300

    cd web logo and name

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,12 @@
 
 
-import codingHubLog from "../assets/imgs/CodingHub.png"
+
 import igIco from "../assets/imgs/ig-instagram-icon.svg"
 import linkedinIco from "../assets/imgs/linkedin-app-icon.svg"
 import githubIco from "../assets/imgs/github-icon.svg"
 import gmailIco from "../assets/imgs/gmail-icon.svg"
 import tailwindIco from "../assets/imgs/tailwind-css-svgrepo-com.svg"
-import codingHubProject from "../assets/imgs/CHproject.png";
+
 import crossIcon from "../assets/imgs/icons8-cross-mark-64.png";
 import  menu  from "../assets/imgs/icons8-menu.svg";
 
@@ -14,8 +14,8 @@ import  menu  from "../assets/imgs/icons8-menu.svg";
 
 export const crossIconImg = crossIcon;
 export const menuIcon = menu;
-export const codingHubProjectImg = codingHubProject;
-export const codingHubLogo = codingHubLog;
+
+
 export const igIcon = igIco;
 export const linkedinIcon = linkedinIco;
 export const githubIcon = githubIco;
